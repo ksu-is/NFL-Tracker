974210b05a210eaddac21e05e306458ad39917d3
Vdonati1 <111438049+Vdonati1@users.noreply.github.com>
2022-11-20 22:14:11 -0500

    Update NFL_Tracker.py

--- NFL_Tracker.py
+++ NFL_Tracker.py
@@ -1,1 +1,6 @@
 
+MAIN_URL = 'http://www.espn.com/nfl/'
+PLAYER_LIST_URL = 'http://www.espn.com/nfl/players'
+PLAYER_PROFILE_URL = 'https://www.espn.com/nfl/player/_/id/[ID]/[player]'
+PLAYER_GAMELOG_URL = 'https://www.espn.com/nfl/player/gamelog/_/id/[ID]/[player]'
+
