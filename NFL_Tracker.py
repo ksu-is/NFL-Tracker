1cca6fd4f0144a78c8dd1c92cd04a3c64bd3425a
Vdonati1 <111438049+Vdonati1@users.noreply.github.com>
2022-11-21 18:37:29 -0500

    Update NFL_Tracker.py

--- NFL_Tracker.py
+++ NFL_Tracker.py
@@ -1,6 +1,23 @@
+import
+
+
+
+
+
 
 MAIN_URL = 'http://www.espn.com/nfl/'
 PLAYER_LIST_URL = 'http://www.espn.com/nfl/players'
 PLAYERS_ID_URL = 'https://www.espn.com/nfl/player/_/id/[ID]/[player]'
 PLAYERS_GAMELOG_URL = 'https://www.espn.com/nfl/player/gamelog/_/id/[ID]/[player]'
 
+class 
+  
+class NFLQuarterbackStats(NFLStats):  
+    def keys(): 
+        return [ 'id', 'player_id', 'GP', 'CMP', 'ATT', 'YDS', 'TDS', 'INT', 'RTG', 'QBR']
+class NFLRunningBackStats(NFLStats):
+    def keys(): 
+        return [ 'id', 'player_id', 'ATT', 'YDS', 'AVG', 'REC', 'YDS', 'AVG', 'TDS', 'LNG', 'FUM', 'LST']
+class NFLWideReceiverStats(NFLStats):
+    def keys(): 
+        return ['id', 'player_id', 'REC', 'TGTS', 'YDS', 'AVG', 'TD', 'LNG', 'FD', 'FUM', 'LST']
